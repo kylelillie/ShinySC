a2ee8614101d1942a6ed0f7a0601cc4e0193edab
Kyle Lillie <kylelillie@gmail.com>
2025-12-05 08:19:27 -0700

    still learning

--- ShinySC/__init__.py
+++ ShinySC/__init__.py
@@ -1,2 +1,8 @@
 import ShinySC
-ShinySC.main()
+from .main import describe
+from .main import get_table
+from .main import instructions
+from .main import simple_metadata
+from .main import full_metadata
+
+__all__ = ["describe", "get_table","instructions","simple_metadata","full_metadata"]
