f39d73f8257f1cef4eab5384959c5fcadf31ea6c
Kyle Lillie <kylelillie@gmail.com>
2025-12-08 20:33:53 -0700

    search mostly works

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ShinySC',
-    version='0.1.5',
+    version='0.1.6',
     author='Kyle Lillie',
     description='This helps you build queries to download custom datatables from Statistics Canada. Ideal for use in data pipelines.',
     packages=find_packages(),
